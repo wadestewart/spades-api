e287cd49ad3f25179f42db0fa243d8f528430ecd
Wade Stewart <stew.wade@gmail.com>
2022-04-17 20:50:27 -0400

    emitting users to lounge

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const socketIo = require('socket.io');
 const http = require('http');
-const PORT = process.env.PORT || 3050;
+const IO_PORT = process.env.PORT || 3050;
+const APP_PORT = process.env.PORT || 3051;
 
 // create the app
 const app = express();
@@ -12,22 +13,28 @@ const server = http.createServer(app);
 
 const io = socketIo(server, { 
     cors: {
-      origin: "http://localhost:3000"
+        origin: 'http://localhost:3000'
     }
 }) //in case server and client run on different urls
 
+const players = [];
 // set up the socket connection
 io.on('connection', (socket) => {
     // emitting the socket id to the client
-    socket.emit("userSocket", socket.id);
+    socket.emit('userSocket', socket.id);
 
-    // receiving the user's name 
-    socket.on('player', player => {
-        console.log('a user connected!', player);
+    // receiving the player object, and pushing it into the players array
+    socket.on('player', (player, callback) => {
+        // push the new player into the 
+        players.push(player);
+        // callback(player);
+        // console.log(players);
+        io.emit('players', players);
     })
 });
 
 // listen for changes on app's port
-server.listen(PORT, function() {
-    console.log(`✅ PORT: ${PORT} 🌟`);
+server.listen(IO_PORT, function() {
+    console.log(`✅ PORT: ${IO_PORT} 🌟`);
 });
+
